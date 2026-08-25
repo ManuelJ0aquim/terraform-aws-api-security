fedbac9a473051f71e527369ab85ed8b364c7bc6
Manuel Joaquim <marquessanches007@gmail.com>
2026-08-25 18:34:32 +0100

    feat(infra): add PostgreSQL RDS instance

--- infra/main.tf
+++ infra/main.tf
@@ -8,11 +8,6 @@ terraform {
   }
 }
 
-resource "aws_instance" "test_cost_ec2" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t3.medium"
-}
-
 provider "aws" {
   region = var.aws_region
 
